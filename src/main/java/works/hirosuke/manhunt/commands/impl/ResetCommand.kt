1ca22d029f46d968fa1b1d60714fcb931d343faf
Hirosuke <51872161+Hirosukt@users.noreply.github.com>
2022-07-03 02:47:06 +0900

    remove waste processes

--- src/main/java/works/hirosuke/manhunt/commands/impl/ResetCommand.kt
+++ src/main/java/works/hirosuke/manhunt/commands/impl/ResetCommand.kt
@@ -1,9 +1,8 @@
 package works.hirosuke.manhunt.commands.impl
 
 import org.bukkit.command.CommandSender
-import org.bukkit.entity.Entity
+import org.bukkit.entity.Item
 import org.bukkit.entity.Player
-import org.bukkit.scoreboard.Team
 import works.hirosuke.manhunt.commands.Command
 
 object ResetCommand : Command("reset") {
@@ -12,12 +11,10 @@ object ResetCommand : Command("reset") {
         if (sender !is Player) return
         sender.server.onlinePlayers.filterNot { it == sender }.forEach {
             it.inventory.clear()
-            it.bedSpawnLocation = sender.location
             it.health = 0.0
-            it.scoreboard.teams.forEach(Team::unregister)
         }
 
-        sender.world.entities.forEach(Entity::remove)
+        sender.world.entities.filterIsInstance(Item::class.java).forEach(Item::remove)
     }
 
     override fun onTabComplete(sender: CommandSender, label: String, args: Array<out String>): List<String>? {
